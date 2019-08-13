42f873c15cd44803e94b7309cd467b71451a7cdf
Saar Shen <saars@microsoft.com>
2019-08-12 17:15:51 -0700

    Add Profiles

--- src/Tools/dotnet-trace-hosted/Startup.cs
+++ src/Tools/dotnet-trace-hosted/Startup.cs
@@ -26,6 +26,7 @@ namespace HostedTrace
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IProfileService, ProfileService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
