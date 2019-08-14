1d39052567258dbb60b2416c10d75e5e2b2fdc57
Saar Shen <saars@microsoft.com>
2019-08-14 08:14:41 -0700

    List sessions

---
+++ src/Tools/dotnet-trace-hosted-webui/src/Components/TraceSessions.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { TraceSession } from '../Models/TraceSession';
+
+interface TraceSessionsProps {
+    traceSessions: TraceSession[] | undefined
+}
+
+export default class TraceSessions extends Component<TraceSessionsProps, {}>{
+    render() {
+        let list;
+        if (this.props.traceSessions === undefined || this.props.traceSessions.length === 0) {
+            list = <div>There's no trace sessions.</div>
+        }
+        else {
+            list = <div>
+                {this.props.traceSessions.map((session, index) => {
+                    return (<div key={index}>
+                        <span>ProcessId:</span>
+                        <span>{session.processId}</span>&nbsp;
+                        <span>SessionId:</span>
+                        <span>{session.sessionId}</span>
+                    </div>);
+                })}
+            </div>
+        }
+        return (<div>
+            <h2>Trace Sessions</h2>
+            {list}
+        </div>);
+    }
+}
