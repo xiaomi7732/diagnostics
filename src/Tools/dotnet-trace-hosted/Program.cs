bf268952cbdaf3cf5f6ee3544a0004032d614fd7
Saar Shen <saars@microsoft.com>
2019-08-14 18:00:30 -0700

    Specify port of the backend to 9400

--- src/Tools/dotnet-trace-hosted/Program.cs
+++ src/Tools/dotnet-trace-hosted/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HostedTrace
 {
@@ -20,7 +14,7 @@ namespace HostedTrace
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("https://*:9400");
                 });
     }
 }
