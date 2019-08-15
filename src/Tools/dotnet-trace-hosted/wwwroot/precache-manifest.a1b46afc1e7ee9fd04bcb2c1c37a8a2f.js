0e3e1eae67d253f4da76f486965032dc9ae7734a
Saar Shen <saars@microsoft.com>
2019-08-14 18:50:29 -0700

    Serve the client

---
+++ src/Tools/dotnet-trace-hosted/wwwroot/precache-manifest.a1b46afc1e7ee9fd04bcb2c1c37a8a2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e040ff9f393b7192de12bb41bb276c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc59c5a6cbd1baaf218",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9309f9eb149f4bdfe813",
+    "url": "/static/js/2.e5a87e08.chunk.js"
+  },
+  {
+    "revision": "0bc59c5a6cbd1baaf218",
+    "url": "/static/js/main.0fc1e543.chunk.js"
+  },
+  {
+    "revision": "e2364413a78e41a89b11",
+    "url": "/static/js/runtime~main.09d06ed7.js"
+  }
+]);
