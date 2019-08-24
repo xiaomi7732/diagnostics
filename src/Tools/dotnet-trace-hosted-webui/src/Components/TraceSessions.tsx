265bcbdc389712b8ca63cb4d9773da50d992dfcf
Saar Shen <saars@microsoft.com>
2019-08-23 19:26:20 -0700

    Stylize the UI more

--- src/Tools/dotnet-trace-hosted-webui/src/Components/TraceSessions.tsx
+++ src/Tools/dotnet-trace-hosted-webui/src/Components/TraceSessions.tsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import { TraceSession } from '../Models/TraceSession';
+import './TraceSessions.css';
 
 interface TraceSessionsProps {
     loadTraceSessionsAsync: () => Promise<void>;
@@ -17,13 +18,17 @@ export default class TraceSessions extends PureComponent<TraceSessionsProps, {}>
         }
         else {
             len = this.props.traceSessions.length;
-            list = <div>
+            list = <div className='session-container'>
                 {this.props.traceSessions.map((session, index) => {
-                    return (<div key={index}>
-                        <span>ProcessId:</span>
-                        <span>{session.processId}</span>&nbsp;
-                        <span>SessionId:</span>
-                        <span>{session.sessionId}</span>&nbsp;
+                    return (<div className='session-line' key={index}>
+                        <div className='process-id-part'>
+                            <span className='bold-text'>ProcessId:</span>
+                            <span>{session.processId}</span>&nbsp;
+                        </div>
+                        <div className='session-id-part'>
+                            <span className='bold-text'>SessionId:</span>
+                            <span>{session.sessionId}</span>&nbsp;
+                        </div>
                         <input className='button' type='button' value='Stop Profiling' onClick={() => {
                             console.debug(`Stopping profiler: ${session.processId}:${session.sessionId}`);
                             this.props.stopProfilingAsync(session.processId, session.sessionId);
@@ -35,7 +40,7 @@ export default class TraceSessions extends PureComponent<TraceSessionsProps, {}>
         return (<div>
             <h2>Trace Sessions ({len})</h2>
             {list}
-            <input className='button' type='button' value='Refresh' onClick={this.props.loadTraceSessionsAsync} />
+            <input className='button' type='button' value='&#x1f5d8; Refresh' onClick={this.props.loadTraceSessionsAsync} />
         </div>);
     }
 }
