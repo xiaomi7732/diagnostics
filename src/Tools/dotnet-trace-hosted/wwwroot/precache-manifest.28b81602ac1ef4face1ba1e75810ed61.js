1b412a871395bfa27361899ce164f9d6da851b69
Saar Shen <saars@microsoft.com>
2019-08-27 18:12:48 -0700

    Integrate new frontend

--- src/Tools/dotnet-trace-hosted/wwwroot/precache-manifest.28b81602ac1ef4face1ba1e75810ed61.js
+++ src/Tools/dotnet-trace-hosted/wwwroot/precache-manifest.28b81602ac1ef4face1ba1e75810ed61.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "184d92f2a4715b1c5e25f171d23409a4",
+    "revision": "d1c5086296cff8c8c91ee9971fa6daa4",
     "url": "/index.html"
   },
   {
-    "revision": "a5dd514a4d2e8e0ef706",
-    "url": "/static/css/main.76e27cc9.chunk.css"
+    "revision": "e92c8ebb42fb77fa3b0c",
+    "url": "/static/css/main.3906f0f3.chunk.css"
   },
   {
     "revision": "9309f9eb149f4bdfe813",
     "url": "/static/js/2.e5a87e08.chunk.js"
   },
   {
-    "revision": "a5dd514a4d2e8e0ef706",
-    "url": "/static/js/main.b37ff524.chunk.js"
+    "revision": "e92c8ebb42fb77fa3b0c",
+    "url": "/static/js/main.bd2eed66.chunk.js"
   },
   {
     "revision": "e2364413a78e41a89b11",
