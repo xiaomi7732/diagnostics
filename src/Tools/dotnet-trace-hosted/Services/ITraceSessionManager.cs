e25755e902dc15f2fb203f62a0944876f219fd41
Saar Shen <saars@microsoft.com>
2019-09-06 18:33:38 -0700

    Generize trace session mamager

--- src/Tools/dotnet-trace-hosted/Services/ITraceSessionManager.cs
+++ src/Tools/dotnet-trace-hosted/Services/ITraceSessionManager.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
-
 namespace HostedTrace
 {
     public interface ITraceSessionManager
     {
         string ListAll();
-        bool TryAdd(TraceSession session);
-        bool TryRemove(TraceSessionId spec, out TraceSession session);
+        bool TryAdd(TraceSessionId session);
+        bool TryRemove<T>(TraceSessionId spec, out T session)
+            where T : TraceSessionId;
     }
 }
