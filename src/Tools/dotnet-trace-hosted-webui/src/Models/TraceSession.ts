e589bfe63364766f91976a1f40e0def81a473d34
Saar Shen <saars@microsoft.com>
2019-09-08 18:48:58 -0700

    Fix issue: trace session type is not specified

--- src/Tools/dotnet-trace-hosted-webui/src/Models/TraceSession.ts
+++ src/Tools/dotnet-trace-hosted-webui/src/Models/TraceSession.ts
@@ -2,5 +2,6 @@ export class TraceSession {
     constructor(
         public processId: number,
         public sessionId: number,
+        public type: number,
     ) { };
 }
--- src/Tools/dotnet-trace-hosted/Models/TraceSessions/ProfileTraceSession.cs
+++ src/Tools/dotnet-trace-hosted/Models/TraceSessions/ProfileTraceSession.cs
@@ -6,6 +6,11 @@ namespace HostedTrace
 {
     public class ProfileTraceSession : TraceSessionId
     {
+        public ProfileTraceSession()
+        {
+            this.Type = TraceSessionType.Profile;
+        }
+
         public Task Task { get; set; }
         public CancellationTokenSource CancellationTokenSource { get; set; }
         public Stream TraceStream { get; set; }
