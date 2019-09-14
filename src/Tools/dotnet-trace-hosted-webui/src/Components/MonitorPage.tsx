277eabf1c96a79c6f939ff19e3f188ed3b735e46
Saar Shen <saars@microsoft.com>
2019-09-14 15:46:12 -0700

    Add start stop profiling to monitor screen

--- src/Tools/dotnet-trace-hosted-webui/src/Components/MonitorPage.tsx
+++ src/Tools/dotnet-trace-hosted-webui/src/Components/MonitorPage.tsx
@@ -2,15 +2,19 @@ import * as React from 'react';
 import MonitorViz from './MonitorViz';
 import { MonitorReport } from '../Models/MonitorReport';
 import './MonitorPage.css';
+import { TraceSession } from '../Models/TraceSession';
 
 interface MonitorPageProps {
     processId: number;
     sessionId: number;
     isDumping: boolean;
+    traceSessionArray: TraceSession[] | undefined;
 
     exitMonitor: () => void;
     getReportAsync: () => Promise<MonitorReport | undefined>;
     takeDumpAsync: (processId: number, isMini: boolean) => Promise<any>;
+    startProfilingAsync: (processId: number) => Promise<boolean>;
+    stopProfilingAsync: (processId: number, sessionId: number) => Promise<boolean>;
 }
 const MonitorPage: React.FC<MonitorPageProps> = ({ processId,
     sessionId,
@@ -18,8 +22,13 @@ const MonitorPage: React.FC<MonitorPageProps> = ({ processId,
     exitMonitor,
     takeDumpAsync,
     isDumping,
+    traceSessionArray,
+    startProfilingAsync,
+    stopProfilingAsync,
 }) => {
     const dumpButtonClassName: string = 'button' + (isDumping ? ' disabled' : '');
+    const profilingSession = traceSessionArray === undefined ? undefined :
+        traceSessionArray.find(session => session.processId === processId && session.processId !== sessionId && session.type === 0);
     return <div className='monitor-page'>
         <div className='header'>
             <h2>Monitoring</h2>
@@ -30,6 +39,18 @@ const MonitorPage: React.FC<MonitorPageProps> = ({ processId,
             <input type='button' value='Mini Dump' className={dumpButtonClassName} onClick={async () => await takeDumpAsync(processId, true)}
                 disabled={isDumping} />
 
+            {!!profilingSession &&
+                <input type='button' value='Stop Profiling' className={dumpButtonClassName} onClick={async () => {
+                    if (profilingSession !== undefined) {
+                        await stopProfilingAsync(profilingSession.processId, profilingSession.sessionId);
+                    }
+                }}
+                />}
+            {!profilingSession &&
+                <input type='button' value='Start Profiling' className={dumpButtonClassName} onClick={async () => {
+                    await startProfilingAsync(processId);
+                }}
+                />}
             <input type='button' value='Back' className='button' onClick={exitMonitor} />
         </div>
         <div className='viz'>
