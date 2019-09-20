1e463aaf47bd804e0fee5c18341d7177356e5d0a
Saar Shen <saars@microsoft.com>
2019-09-19 18:16:06 -0700

    Enabling Applicaiton Insights for backend

--- src/Tools/dotnet-trace-hosted/Startup.cs
+++ src/Tools/dotnet-trace-hosted/Startup.cs
@@ -19,6 +19,7 @@ namespace HostedTrace
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddSignalR();
             services.AddControllers();
             services.AddScoped<IProfileService, ProfileService>();
